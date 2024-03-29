fd1da31b929b09b0f22ca28663f1d4ebfd9f0456
= <=>
2024-03-29 18:44:12 +0530

    Task

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Hexleap Private Limited",
-  description: "INternship Task",
+  title: "Internship Task | Hexleap Private Limited",
+  description: "Internship Task",
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Component1 from './components/Component1'
+import Component2 from './components/Component2'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div className='dark:text-white dark:bg-neutral-950'>
+      <Component1 />
+      <Component2 />
+    </div>
   )
 }
